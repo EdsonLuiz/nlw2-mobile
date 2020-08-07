50bfeaf31f73c2771d839bcd1e1e803f2bfa2075
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-07 15:24:57 -0300

    refactor: linking to whatsapp teacher

--- src/components/TeacherItem/TeacherItem.tsx
+++ src/components/TeacherItem/TeacherItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text, Image} from 'react-native'
+import {View, Text, Image, Linking} from 'react-native'
 
 import styles from './styles'
 import {RectButton} from 'react-native-gesture-handler'
@@ -23,6 +23,11 @@ interface TeacherItemProps {
 }
 
 const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
+
+  function  handleLinkToWhatsapp() {
+    Linking.openURL(`whatsapp://ssend?phone=${teacher.whatsapp}`)
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
@@ -50,7 +55,7 @@ const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
             <Image source={unfavoriteIcon} />
           </RectButton>
 
-          <RectButton style={styles.contactButton}>
+          <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
             <Image source={whatsappIcon} />
             <Text style={styles.contactButtonText}>Entrar em contato</Text>
           </RectButton>
