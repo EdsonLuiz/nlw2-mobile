b1439d33546532350688dddf915663a93bd70188
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-07 13:41:35 -0300

    feat: add button to toggle visibility of filters

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {ReactNode} from 'react'
 import {View, Text, Image} from 'react-native'
 import {BorderlessButton} from 'react-native-gesture-handler'
 
@@ -10,10 +10,11 @@ import {useNavigation} from '@react-navigation/native'
 
 interface PageHeaderProps {
   title: string;
+  headerRight?: ReactNode
 }
 
 
-const PageHeader:React.FC<PageHeaderProps> = ({title}) => {
+const PageHeader:React.FC<PageHeaderProps> = ({title, headerRight, children}) => {
 
   const {navigate} = useNavigation()
 
@@ -33,7 +34,13 @@ const PageHeader:React.FC<PageHeaderProps> = ({title}) => {
         <Image source={logo} resizeMode="contain" />
       </View>
 
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
+
+      {children}
+
     </View>
   )
 }
--- src/pages/TeacherList/TeacherList.tsx
+++ src/pages/TeacherList/TeacherList.tsx
@@ -1,15 +1,63 @@
-import React from 'react'
-import {View, Text, ScrollView} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, ScrollView, TextInput} from 'react-native'
+import {Feather} from '@expo/vector-icons'
 
 
 import styles from './styles'
 import {PageHeader} from '../../components/PageHeader'
 import {TeacherItem} from '../../components/TeacherItem'
+import {BorderlessButton, RectButton} from 'react-native-gesture-handler'
 
 const TeacherList:React.FC = () => {
+
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false)
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible)
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponiveis" />     
+      <PageHeader 
+        title="Proffys disponiveis" 
+        headerRight={(
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name="filter" size={20} color="#fff" />
+          </BorderlessButton>
+        )}>
+        {isFiltersVisible && (
+          <View style={styles.serachForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput 
+              style={styles.input}
+              placeholderTextColor='#c1bccc'
+              placeholder="Qual a matéria" />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput 
+                  style={styles.input}
+                  placeholderTextColor='#c1bccc'
+                  placeholder="Qual o dia" />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput 
+                  style={styles.input}
+                  placeholderTextColor='#c1bccc'
+                  placeholder="Qual horário" />
+              </View>
+
+            </View>
+            <RectButton style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+
+          </View>
+        )}
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
