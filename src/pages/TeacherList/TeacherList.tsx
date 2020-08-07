30a9f66db2ad62040ba70a464a9b636bda19c5a7
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-07 11:45:45 -0300

    refactor: use TeacherItem in list

---
+++ src/pages/TeacherList/TeacherList.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {View, Text, ScrollView} from 'react-native'
+
+
+import styles from './styles'
+import {PageHeader} from '../../components/PageHeader'
+import {TeacherItem} from '../../components/TeacherItem'
+
+const TeacherList:React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponiveis" />     
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 24
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
+
+    </View>
+  )
+}
+
+export default TeacherList
